4714eda6a4ba1c94fd2adea5b4a9d6fb93496bdf
kimjy0126 <kimjy0126@skku.edu>
2021-03-04 18:40:07 +0900

    test 수정

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 pub mod mem_reader;
 pub mod types;
 
+mod tests;
+
 pub use mem_reader::MemReader;
---
+++ src/tests.rs
@@ -0,0 +1,16 @@
+#[cfg(test)]
+use super::types::*;
+
+#[test]
+fn types_from_u32() {
+    let bytes: ByteValue = ByteValue::from_u32(0xdeadbeef);
+    assert_eq!(bytes, ByteValue::from_vec(vec![0xef, 0xbe, 0xad, 0xde]));
+}
+
+#[test]
+fn types_to_u32() {
+    let bytes: ByteValue = ByteValue::from_vec(vec![0xef, 0xbe, 0xad, 0xde, 0xfe, 0xca, 0x21, 0x20]);
+    let result: Vec<u32> = bytes.to_u32();
+
+    assert_eq!(result, vec![0xdeadbeef, 0x2021cafe]);
+}
