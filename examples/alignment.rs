00780a801c712b4450d796a1127bfb60206ae4eb
kimjy0126 <kimjy0126@skku.edu>
2021-03-07 16:59:22 +0900

    중간 저장: 예시 추가: alignment

--- examples/alignment.rs
+++ examples/alignment.rs
@@ -7,8 +7,8 @@ fn main() {
     let v: Vec<u8> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
     let pv: ByteAddress = ByteAddress::from(&v as *const Vec<u8> as u64);
 
-    let reader: MemReader = MemReader::new().set_alignment(6);
+    let reader: MemReader = MemReader::new().set_alignment(5);
     let ppv = ByteAddress::from(reader.read(pv, 0, 8)[0]);
 
-    reader.print_with_range(ppv, 0, 12);
+    reader.print_with_range(ppv, 0, 15);
 }
